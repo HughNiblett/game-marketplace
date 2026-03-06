d3e37a25bf5b3085d5771b017163ec4b5ac231da
Hugh-Niblett_sita <hughniblett03@gmail.com>
2026-03-06 17:31:31 +0000

    [#7] Created State API template project using spring initializr

---
+++ state/src/test/java/com/github/hughniblett/marketplace/state/StateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hughniblett.marketplace.state;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
