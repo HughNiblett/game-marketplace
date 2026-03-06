b583d80d183f9fddb86ba10b95a33dda3469223b
Hugh-Niblett_sita <hughniblett03@gmail.com>
2026-03-06 14:26:03 +0000

    [#10] Created Spring Boot project using Spring Initializr as a template for the Listings API

---
+++ listings/src/test/java/com/github/hughniblett/marketplace/listings/ListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hughniblett.marketplace.listings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
