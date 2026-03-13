af41abfb212e31a5eb6a8aa1bdff1ac330c170ee
Hugh-Niblett_sita <hughniblett03@gmail.com>
2026-03-13 21:35:15 +0000

    [#28] Created item entity object

---
+++ state/src/main/java/com/github/hughniblett/marketplace/state/entity/Item.java
@@ -0,0 +1,24 @@
+package com.github.hughniblett.marketplace.state.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Item {
+
+  @Id
+  @GeneratedValue
+  private String id;
+  private String name;
+  private String description;
+
+  public Item() {}
+
+  public Item(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+}
